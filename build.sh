d67035acc5b34b93887b54ac02dd058023dee73f
Anton <a.tashchilin@bk.ru>
2023-11-23 22:05:19 +0400

    fixs bags in build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd $(dirname $0) || exit 1
 cd consumer
-DOCKER_BUILDKIT=1 docker build -f build-docker/Dockerfile -t serv/consumer .
+DOCKER_BUILDKIT=1 docker build -t serv/consumer .
 cd ..
 cd producer
-DOCKER_BUILDKIT=1 docker build -f build-docker/Dockerfile -t serv/producer .
+DOCKER_BUILDKIT=1 docker build -t serv/producer .
 cd ..
