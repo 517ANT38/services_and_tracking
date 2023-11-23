51248c036f5a78e4a85aeeb76047a5f28e41952d
Anton <a.tashchilin@bk.ru>
2023-11-23 15:31:15 +0400

    fixs bags

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd $(dirname $0) || exit 1
 cd kafka-consumer
-docker build -f build-docker/Dockerfile -t serv/consumer .
+DOCKER_BUILDKIT=1 docker build -f build-docker/Dockerfile -t serv/consumer .
 cd ..
 cd kafka-producer
-docker build -f build-docker/Dockerfile -t serv/producer .
+DOCKER_BUILDKIT=1 docker build -f build-docker/Dockerfile -t serv/producer .
 cd ..
