48dc1130d59ce93dfb671655a6ba81882ca8736a
Anton <a.tashchilin@bk.ru>
2023-11-23 21:57:40 +0400

    fixs bags

--- producer/src/main/java/com/anton/producer/service/WeatherService.java
+++ producer/src/main/java/com/anton/producer/service/WeatherService.java
@@ -3,11 +3,10 @@ package com.anton.producer.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.anton.producer.domain.WeatherData;
 import com.anton.producer.service.producer.Producer;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Slf4j
+
 @Service
 public class WeatherService {
 
