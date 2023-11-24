ef47061dd61945f51f03e2e90c54b7bdaa94f868
517ANT38 <a.tashchilin@bk.ru>
2023-11-24 13:24:16 +0300

    add folder in producer

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd $(dirname $0)/.. || exit 1
-cd consumer
-DOCKER_BUILDKIT=1 docker build -t serv/consumer .
-cd ..
+# cd consumer
+# DOCKER_BUILDKIT=1 docker build -t serv/consumer .
+# cd ..
 cd producer
-DOCKER_BUILDKIT=1 docker build -t serv/producer .
+DOCKER_BUILDKIT=1 docker build -f build-elements/Dockerfile -t serv/producer1 .
 cd ..
