d35331a2623646a5ed4372a2451f28785a1fd93d
517ANT38 <a.tashchilin@bk.ru>
2023-11-26 06:22:42 +0300

    add updates

--- producer/src/main/java/com/anton/producer/domain/WeatherData.java
+++ producer/src/main/java/com/anton/producer/domain/WeatherData.java
@@ -1,16 +1,19 @@
 package com.anton.producer.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.util.Date;
 
 @Data
 @Value
+@AllArgsConstructor
 public class WeatherData {
-    String desd;
-    Date date;
-    Double tempr;
-    Double spendWinter;
-    String item;
+    private String desd;
+    private Date date;
+    private Double tempr;
+    private Double spendWinter;
+    private String item;
 }
--- producer/src/main/java/com/anton/producer/service/scheduler/Scheduler.java
+++ producer/src/main/java/com/anton/producer/service/scheduler/Scheduler.java
@@ -1,5 +1,6 @@
 package com.anton.producer.service.scheduler;
 
+import com.anton.producer.domain.WeatherData;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -9,6 +10,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
 import java.util.Map;
 import java.util.Random;
 
@@ -21,7 +23,9 @@ public class Scheduler {
     private final KafkaTemplate<String, String> kafkaTemplate;
     private static final String orderTopic = "${topic.name}";
     private final ObjectMapper objectMapper;
-    private Integer count = 0;
+    private Long count1 = 0L;
+    private Long count2 = 0L;
+
 
     @Autowired
     public Scheduler(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper,Random r) {
@@ -33,10 +37,10 @@ public class Scheduler {
     @Scheduled(fixedRate = 1000)
     @Observed(name="scheduled_send_weather_data")
     public void sendMessage() throws JsonProcessingException {
-        count++;
+        count1++;
         String dataAsMessage = objectMapper.writeValueAsString(Map.of(
-                    "vars","val" + count,
-                    "zar", "zar" +count,
+                    "vars","val" + count1,
+                    "zar", "zar" +count1,
                         "vd",String.valueOf(random.nextExponential())
         ));
         kafkaTemplate.send("t.const.weather", dataAsMessage);
@@ -45,10 +49,10 @@ public class Scheduler {
     @Scheduled(fixedRate = 2000)
     @Observed(name="scheduled_send_weather_data_two")
     public void sendMessageTwo() throws JsonProcessingException {
-        count++;
+        count1++;
         String dataAsMessage = objectMapper.writeValueAsString(Map.of(
-                    "vars","val" +random.nextInt(count),
-                    "zar", "zar" +random.nextInt(count),
+                    "vars","val" +random.nextLong(count1),
+                    "zar", "zar" +random.nextLong(count1),
                         "vd",String.valueOf(random.nextExponential())
         ));
         kafkaTemplate.send("t.const.weather", dataAsMessage);
@@ -57,18 +61,58 @@ public class Scheduler {
     @Scheduled(fixedRate = 3000)
     @Observed(name="scheduled_send_weather_data_three")
     public void sendMessageThree() throws JsonProcessingException {
-        count++;
+        count1++;
         String dataAsMessage = objectMapper.writeValueAsString(Map.of(
-                    "vars","val" +random.nextInt(count)*random.nextDouble(),
-                    "zar", "zar" +random.nextInt(count)*3,
+                    "vars","val" +random.nextLong(count1)*random.nextDouble(),
+                    "zar", "zar" +random.nextLong(count1)*3,
                         "vd",String.valueOf(random.nextExponential())
         ));
         kafkaTemplate.send("t.const.weather", dataAsMessage);
     }
 
+    @Scheduled(fixedRate = 1000)
+    @Observed(name="send_message_wea_t")
+    public void sendMessageWea() throws JsonProcessingException {
+        count2++;
+        String dataAsMessage = objectMapper.writeValueAsString(new WeatherData(
+            "dfggdg" + count2,
+            new Date(System.currentTimeMillis()),
+            90.467 + count2,
+            90.8 + count2,
+            "ghadjs" + count2
+        ));
+        kafkaTemplate.send(orderTopic, dataAsMessage);
+    }
+
+    @Scheduled(fixedRate = 2000)
+    @Observed(name="send_message_wea_t2")
+    public void sendMessageWeaTwo() throws JsonProcessingException {
+        count2++;
+        String dataAsMessage = objectMapper.writeValueAsString(new WeatherData(
+            "wewqg" + count2,
+            new Date(System.currentTimeMillis()),
+            4560.467 + count2,
+            546.8 + count2,
+            "wqewq" + count2
+        ));
+        kafkaTemplate.send(orderTopic, dataAsMessage);
+    }
+
+    @Scheduled(fixedRate = 3000)
+    @Observed(name="send_message_wea_t3")
+    public void sendMessageWeaThree() throws JsonProcessingException {
+        count2++;
+        String dataAsMessage = objectMapper.writeValueAsString(new WeatherData(
+            "hjwewqg" + count2,
+            new Date(System.currentTimeMillis()),
+            4780.467 + count2,
+            523.8 + count2,
+            "wqewq" + count2
+        ));
+        kafkaTemplate.send(orderTopic, dataAsMessage);
+    }
 
 
 
-    
 
 }
