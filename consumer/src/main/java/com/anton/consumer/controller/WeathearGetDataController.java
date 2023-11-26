80ca18fc94a9feb2e1d403ab0d64920b7225bccc
517ANT38 <a.tashchilin@bk.ru>
2023-11-26 08:33:47 +0300

    add consumer controller

---
+++ consumer/src/main/java/com/anton/consumer/controller/WeathearGetDataController.java
@@ -0,0 +1,46 @@
+package com.anton.consumer.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anton.consumer.domain.dto.ConstWeatherDataDto;
+import com.anton.consumer.domain.dto.WeatherDataDto;
+import com.anton.consumer.service.WeatherService;
+
+import io.micrometer.observation.annotation.Observed;
+import lombok.AllArgsConstructor;
+
+@RequestMapping("/getDataOrConstDataWeather")
+@RestController
+@AllArgsConstructor
+public class WeathearGetDataController {
+    
+    private final WeatherService wService;
+
+    @GetMapping("/data")
+    @Observed(name = "weathear_data_get_all")
+    public ResponseEntity<List<WeatherDataDto>> getDataAll(){
+        return ResponseEntity.ok(wService.selecWeatherDataDtosBy());
+    }
+    @GetMapping("/constData")
+    @Observed(name = "const_weathear_data_get_all")
+    public ResponseEntity<List<ConstWeatherDataDto>> getConstDataAll(){
+        return ResponseEntity.ok(wService.selectConstWeatherDataDtos());
+    }
+    @GetMapping("/data/tempr/{t}")
+    @Observed(name = "weathear_data_get_all")
+    public ResponseEntity<List<WeatherDataDto>> getDataByTempr(@PathVariable("t") Double t){
+        return ResponseEntity.ok(wService.selecWeatherDataDtosByT(t));
+    }
+    @GetMapping("/data/spendWinter/{s}")
+    @Observed(name = "weathear_data_get_all")
+    public ResponseEntity<List<WeatherDataDto>> getDataBySpendWinter(@PathVariable("s") Double s){
+        return ResponseEntity.ok(wService.selecWeatherDataDtosByS(s));
+    }
+
+}
