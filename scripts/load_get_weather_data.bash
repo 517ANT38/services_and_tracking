87585ad821f8fab6c93d6a7aeffaed4564f9f13b
517ANT38 <a.tashchilin@bk.ru>
2023-11-26 11:17:26 +0300

    add get test scripts

---
+++ scripts/load_get_weather_data.bash
@@ -0,0 +1,49 @@
+#!/bin/bash  
+
+function random(){ echo $(($1 + $RANDOM % $2));}
+
+function getDataOrConstDataWeather0(){  
+  for((i=0; i<$1; i++))
+  do
+    curl  -X GET \
+    "http://$2:$3/getDataOrConstDataWeather/constData" \
+    --header 'Accept: */*'> /dev/null 
+  done
+}
+
+function getDataOrConstDataWeather1(){
+    for((i=0; i<$1; i++))
+    do
+    curl  -X GET \
+    "http://$2:$3/getDataOrConstDataWeather/data/tempr/$(random 1 99)" \
+    --header 'Accept: */*' > /dev/null 
+    done
+
+}
+
+function getDataOrConstDataWeather2(){
+    for((i=0; i<$1; i++))
+    do
+        curl  -X GET \
+        "http://$2:$3/getDataOrConstDataWeather/data" \
+        --header 'Accept: */*'> /dev/null 
+    done
+}
+
+function getDataOrConstDataWeather3(){
+    for((i=0; i<$1; i++))
+    do
+    curl  -X GET \
+    "http://$2:$3/getDataOrConstDataWeather/data/spendWinter/$(random 1 99)" \
+    --header 'Accept: */*' > /dev/null 
+    done
+
+} 
+
+getDataOrConstDataWeather0 $3 $1 $2 & 
+
+getDataOrConstDataWeather1 $4 $1 $2 & 
+
+getDataOrConstDataWeather2 $5 $1 $2 & 
+
+getDataOrConstDataWeather3 $6 $1 $2 &
---
+++ scripts/localhost_get_weather_data.bash
@@ -0,0 +1,2 @@
+#!/bin/bash 
+./load_get_weather_data.bash 'localhost' 8081 $1 $2 $3 $4
