95fe8c23dc9c613b69742ac5174b95d061ffcb6b
517ANT38 <a.tashchilin@bk.ru>
2023-11-26 09:30:06 +0300

    add annotation rep

--- consumer/src/main/java/com/anton/consumer/repository/ConstWeatherDataRepository.java
+++ consumer/src/main/java/com/anton/consumer/repository/ConstWeatherDataRepository.java
@@ -1,9 +1,20 @@
 package com.anton.consumer.repository;
 
 import com.anton.consumer.domain.ConstWeatherData;
+import com.anton.consumer.domain.WeatherData;
+
+import io.micrometer.observation.annotation.Observed;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConstWeatherDataRepository extends JpaRepository<ConstWeatherData, Long> {
+    @Observed(name = "findAll_trace")
+    List<ConstWeatherData> findAll();
+
+    @Observed(name = "save_trace")
+    ConstWeatherData save(ConstWeatherData c);
 }
--- consumer/src/main/java/com/anton/consumer/repository/WeatherDataRepository.java
+++ consumer/src/main/java/com/anton/consumer/repository/WeatherDataRepository.java
@@ -9,14 +9,22 @@ import org.springframework.stereotype.Repository;
 
 import com.anton.consumer.domain.WeatherData;
 
+import io.micrometer.observation.annotation.Observed;
+
 @Repository
 public interface WeatherDataRepository extends JpaRepository<WeatherData, Long> {
 
     @Query("select w from WeatherData w where w.tempr <= :t")
+    @Observed(name = "findByTempr_trace")
     List<WeatherData> findByTempr(@Param("t") Double t);
 
     @Query("select w from WeatherData w where w.spendWinter <= :s")
+    @Observed(name = "findBySpendWinter_trace")
     List<WeatherData> findBySpendWinter(@Param("s") Double s);
 
+    @Observed(name = "findAll_trace")
+    List<WeatherData> findAll();
 
+    @Observed(name = "save_trace")
+    WeatherData save(WeatherData w);
 }
