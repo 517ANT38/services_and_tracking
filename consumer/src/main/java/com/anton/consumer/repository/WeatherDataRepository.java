fcd75165b2958d6877d7dc0b92a5d6cc09e120d0
517ANT38 <a.tashchilin@bk.ru>
2023-11-26 08:34:07 +0300

    add method repo cons

--- consumer/src/main/java/com/anton/consumer/repository/WeatherDataRepository.java
+++ consumer/src/main/java/com/anton/consumer/repository/WeatherDataRepository.java
@@ -1,9 +1,22 @@
 package com.anton.consumer.repository;
 
-import com.anton.consumer.domain.WeatherData;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.anton.consumer.domain.WeatherData;
 
 @Repository
 public interface WeatherDataRepository extends JpaRepository<WeatherData, Long> {
+
+    @Query("select w from WeatherData w where w.tempr <= :t")
+    List<WeatherData> findByTempr(@Param("t") Double t);
+
+    @Query("select w from WeatherData w where w.spendWinter <= :s")
+    List<WeatherData> findBySpendWinter(@Param("s") Double s);
+
+
 }
