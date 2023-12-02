45b02f23d5be8e3f8c648c8f60b0e8c01f327723
517ANT8 <a.tashchilin@bk.ru>
2023-12-02 21:04:57 +0300

    add change build elems producer

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)/.. || exit 1
 cd consumer
 DOCKER_BUILDKIT=1 docker build -f build-elements/Dockerfile -t serv/consumer .
-# cd ..
-# cd producer
-# DOCKER_BUILDKIT=1 docker build -f build-elements/Dockerfile -t serv/producer .
-# cd ..
+cd ..
+cd producer
+DOCKER_BUILDKIT=1 docker build -f build-elements/Dockerfile -t serv/producer .
+cd ..
